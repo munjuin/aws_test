b2a6791181894a5d0dba32f1be118b142ee66bf9
munjuin <raiawa1212@gmail.com>
2024-08-26 15:47:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const sequelize = require('sequelize');
+const userModel = require('./models/User')
+const PORT = 8000;
+require('dotenv').config();
+
+//sequelize 연결 설정
+const Sequelize = new sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host:process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+// 모델 초기화
+const User = userModel(Sequelize);
+
+app.use(express.json());
+app.get('/', (req,res)=>{
+  res.send('하이');
+});
+
+app.post('/api/users', async (req, res)=>{
+  try {
+    const {username, email} = req.body;
+    const user = await User.create({username, email});
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: '서버에러'});
+    
+  }
+});
+
+
+Sequelize.sync({force: false}).then(()=>{
+  console.log('테이블생성완료');
+
+  app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+    
+  })
+  
+});
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const {DataTypes, Sequelize} = require('sequelize');
+
+module.exports = (sequelize)=>{
+  return sequelize.define('User', {
+    id:{
+      type:DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+};
